5363ab3c829085e9f22f3b56808954f86d64c423
MarinaNikole <nikamarina222@gmail.com>
2023-10-19 09:58:19 +0200

    changed  NewTask3

--- marinaMelnikova/newTask3.sql
+++ marinaMelnikova/newTask3.sql
@@ -1,5 +1,5 @@
 -- Задача. Вывести ко-во заказов у клиентов из France
-SELECT count(*)
+SELECT count(*) AS total_price
 FROM Orders
 JOIN Customers USING (CustomerID)
 WHERE Customers.Country = 'France'
