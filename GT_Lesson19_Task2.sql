95e926bb1ff67e3d1633c40d3ec4f85c7585ba77
AlexgorAlexgor <alexgor@gmx.de>
2023-10-19 10:31:20 +0200

    GT_Lesson19_Task2_TopCustomers
    GT_Lesson19_Task2_TopCustomers

---
+++ GT_Lesson19_Task2.sql
@@ -0,0 +1,12 @@
+Задача. Вывести ТОП-3 юзеров, у которых больше всего заказов
+SELECT
+	Customers.CustomerName, 
+	COUNT(Orders.OrderID) AS total_orders
+FROM Customers
+
+JOIN Orders USING(CustomerID)
+
+GROUP BY Customers.CustomerID
+
+ORDER BY total_orders DESC
+LIMIT 3
