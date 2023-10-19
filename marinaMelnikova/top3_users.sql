ad218f339625e15508ea7207f405c6061938a55a
MarinaNikole <nikamarina222@gmail.com>
2023-10-19 10:30:15 +0200

    changed new_task/top3_users

--- marinaMelnikova/top3_users.sql
+++ marinaMelnikova/top3_users.sql
@@ -1,8 +1,8 @@
 --Вывести ТОП-3 юзеров, у которых больше всего заказов
 --проекция: имя_юзера, ко_во_заказов
-SELECT CustomerName, COUNT(OrderID) AS total_ordes
+SELECT CustomerName, COUNT(OrderID) AS total_orders
 FROM Orders
 JOIN Customers USING (CustomerID)
-Group BY (CustomerName)
-ORDER BY total_ordes DESC
+Group BY (CustomerID)
+ORDER BY total_orders DESC
 LIMIT 3
