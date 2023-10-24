f2676203609a1593030caaf8a86b8643f76a71bc
AlexgorAlexgor <alexgor@gmx.de>
2023-10-24 14:36:19 +0200

    SummaryTask11

---
+++ MariaDB.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO users(fullname, country)
+VALUES
+	('user 1', 'Germany'),
+    
+	('user 2', 'USA'),
+    ('user 3', 'USA')
+    
+    --Задача. Вывести имена юзеров 1 и 3
+    SELECT fullname from users 
