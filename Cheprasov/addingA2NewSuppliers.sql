bf856a7c7f00f538d5c490afd43da544fb7f93f5
Anton.Cheprasov <anton_cheprasov@icloud.com>
2023-10-31 13:35:54 +0100

    add a 2 Supp

---
+++ Cheprasov/addingA2NewSuppliers.sql
@@ -0,0 +1,6 @@
+-- Задача. Добавить двух поставщиков: из China и из France
+
+INSERT INTO Suppliers(SupplierName, ContactName, Address, City, PostalCode, Country, Phone)
+VALUES
+('Goods', 'Arr', '123 Rui', 'Rennes', '12345', 'France', '(11)243-4404424'),
+('Zhang', 'Zhi Zhung', '11 Lao', 'Harbin', '54321', 'China', '(22)578275-332');
